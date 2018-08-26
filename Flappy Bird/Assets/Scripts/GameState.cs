ef9fd21910db110e505fb510d414a8c3fa7d3fed
Johannes Palvanen <palvanenj@gmail.com>
2018-08-26 19:29:33 +0300

    Movement and score meter

---
+++ Flappy Bird/Assets/Scripts/GameState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour 
+{
+	public Rigidbody2D playerBody;
+	public float deathPosition;
+	private float score;
+	private UIManager uiController;
+
+	void Start()
+	{
+		uiController = GameObject.Find("UI").GetComponent<UIManager>();
+		StartCoroutine(scoreCounter());
+	}
+
+	void Update()
+	{
+		uiController.setScoreText("Score: " + score);
+		if (isDead())
+		{
+			Debug.Log("Player is dead!");
+		}
+	}
+
+	bool isDead()
+	{
+		if(Mathf.Abs(playerBody.position.y) > deathPosition)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	IEnumerator scoreCounter()
+	{
+		while(true)
+		{
+			score += 0.1f;
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+}
---
+++ Flappy Bird/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public Vector2 jumpForce;
+	public float maxSpeed;
+
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () 
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb2d.AddForce(jumpForce, ForceMode2D.Impulse);
+		}
+
+		if (rb2d.velocity.y > maxSpeed)
+		{
+			rb2d.AddForce(new Vector2(0, -Mathf.Abs(maxSpeed - rb2d.velocity.y)));
+		}
+	}
+}
