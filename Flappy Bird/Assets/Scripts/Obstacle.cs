89f293ae97b1b39e52d16ab87e484e061225d893
Johannes Palvanen <palvanenj@gmail.com>
2018-09-03 19:59:11 +0300

    Reworking spawner
    Also added a tree.

---
+++ Flappy Bird/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObstaclePrefs
+{
+	public string name;
+	public Sprite obstacleSprite;
+	public Vector2 startPoint;
+	public float moveSpeed;
+}
+
+public class Obstacle : MonoBehaviour
+{
+	[Header("Obstacles")] 
+	[Tooltip("The randomly spawned obstacles will use this objects components.")] 
+	public GameObject templateObject;
+	[SerializeField, Tooltip("Add all obstacles to this list.")]
+	private ObstaclePrefs[] obstacleList = new ObstaclePrefs[1];
+
+	//This method randomly selects one item from obstaclePrefs and creates an obstacle based on what it receives from there.
+	public GameObject generateObstacle()
+	{
+		System.Random rnd = new System.Random();
+		ObstaclePrefs selectedPrefs = obstacleList[0];
+
+		templateObject.name = "Random " + selectedPrefs.name;
+		templateObject.GetComponent<SpriteRenderer>().sprite = selectedPrefs.obstacleSprite;
+		templateObject.GetComponent<Rigidbody2D>().position = selectedPrefs.startPoint;
+
+		return templateObject;
+	}
+
+	IEnumerator Start()
+	{
+		while(true)
+		{
+			Instantiate(generateObstacle(), new Vector3(40f, 0f, 0f), Quaternion.identity);
+			yield return new WaitForSeconds(5f);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		gameObject.GetComponent<Rigidbody2D>().position += new Vector2(-.1f, 0);
+	}
+}
